be7e2130a4b292b41b8da580c2d3c05142d7e413
DarioGuastella <jimmyguastella@gmail.com>
2024-04-15 17:12:04 +0200

    add Vue

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            deck: []
+
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}).mount('#app')
