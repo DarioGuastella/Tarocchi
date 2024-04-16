caf53e7319d82668ae1d8518fb125b8ea5ccd202
DarioGuastella <jimmyguastella@gmail.com>
2024-04-16 16:40:01 +0200

    add deck

--- js/main.js
+++ js/main.js
@@ -3,7 +3,96 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            deck: []
+            deck: [
+                {
+                    name: "Il Bagatto",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/8/88/Visconti-sforza-01-magician.jpg",
+                },
+                {
+                    name: "La Papessa",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/4/40/Visconti-sforza-02-popess.jpg",
+                },
+                {
+                    name: "L'Imperatrice",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Visconti-sforza-03-empress.jpg",
+                },
+                {
+                    name: "L'Imperatore",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Visconti-sforza-04-emperor.jpg",
+                },
+                {
+                    name: "Il Papa",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/8/8d/Visconti-sforza-05-pope.jpg",
+                },
+                {
+                    name: "Gli Amanti",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/6/60/Visconti-sforza-06-love.jpg",
+                },
+                {
+                    name: "Il Carro",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Visconti-sforza-07-chariot.jpg",
+                },
+                {
+                    name: "La Giustizia",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/0/0d/Visconti-sforza-08-justice.jpg",
+                },
+                {
+                    name: "L'Eremita",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/8/88/Visconti-sforza-09-time.jpg",
+                },
+                {
+                    name: "La Ruota della Fortuna",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/b/b0/Visconti-sforza-10-fortune.jpg",
+                },
+                {
+                    name: "La Forza",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Visconti-sforza-11-fortitude.jpg",
+                },
+                {
+                    name: "L'Appeso",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Visconti-sforza-12-traitor.jpg",
+                },
+                {
+                    name: "La Morte",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Visconti-sforza-13-death.jpg",
+                },
+                {
+                    name: "La Temperanza",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/a/a6/Visconti-sforza-14-temperance.jpg",
+                },
+                {
+                    name: "Il Diavolo",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/RWS_Tarot_15_Devil.jpg/800px-RWS_Tarot_15_Devil.jpg",
+                },
+                {
+                    name: "La Torre",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/RWS_Tarot_16_Tower.jpg/800px-RWS_Tarot_16_Tower.jpg",
+                },
+                {
+                    name: "La Stella",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Visconti-sforza-17-star.jpg",
+                },
+                {
+                    name: "La Luna",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/1/10/Visconti-sforza-18-moon.jpg",
+                },
+                {
+                    name: "Il Sole",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/e/e6/Visconti-sforza-19-sun.jpg",
+                },
+                {
+                    name: "Il Giudizio",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/6/6d/Visconti-sforza-20-judgment.jpg",
+                },
+                {
+                    name: "Il Mondo",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Visconti-sforza-21-world.jpg",
+                },
+                {
+                    name: "Il Matto",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/1/1b/Visconti-sforza-00-fool.jpg",
+                },
+            ]
 
         }
     },
@@ -11,6 +100,12 @@ createApp({
 
     },
     mounted() {
+        /*22 carte
+        0 21
+        mischiare carte
+        pescare 1 sola o 3
+        numero, immagine, nome
+        reset*/
 
     }
 }).mount('#app')
