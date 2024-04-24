15fe53017e1cfeded1f5bd864e25ed4e6443fc37
DarioGuastella <jimmyguastella@gmail.com>
2024-04-24 17:29:58 +0200

    popola array carte selezionate dall'utente max 5 carte

--- js/main.js
+++ js/main.js
@@ -7,96 +7,133 @@ createApp({
                 {
                     name: "Il Bagatto",
                     image: "https://upload.wikimedia.org/wikipedia/commons/8/88/Visconti-sforza-01-magician.jpg",
+                    selected: false
                 },
                 {
                     name: "La Papessa",
                     image: "https://upload.wikimedia.org/wikipedia/commons/4/40/Visconti-sforza-02-popess.jpg",
+                    selected: false,
                 },
                 {
                     name: "L'Imperatrice",
                     image: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Visconti-sforza-03-empress.jpg",
+                    selected: false,
                 },
                 {
                     name: "L'Imperatore",
                     image: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Visconti-sforza-04-emperor.jpg",
+                    selected: false,
                 },
                 {
                     name: "Il Papa",
                     image: "https://upload.wikimedia.org/wikipedia/commons/8/8d/Visconti-sforza-05-pope.jpg",
+                    selected: false,
                 },
                 {
                     name: "Gli Amanti",
                     image: "https://upload.wikimedia.org/wikipedia/commons/6/60/Visconti-sforza-06-love.jpg",
+                    selected: false,
                 },
                 {
                     name: "Il Carro",
                     image: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Visconti-sforza-07-chariot.jpg",
+                    selected: false,
                 },
                 {
                     name: "La Giustizia",
                     image: "https://upload.wikimedia.org/wikipedia/commons/0/0d/Visconti-sforza-08-justice.jpg",
+                    selected: false,
                 },
                 {
                     name: "L'Eremita",
                     image: "https://upload.wikimedia.org/wikipedia/commons/8/88/Visconti-sforza-09-time.jpg",
+                    selected: false,
                 },
                 {
                     name: "La Ruota della Fortuna",
                     image: "https://upload.wikimedia.org/wikipedia/commons/b/b0/Visconti-sforza-10-fortune.jpg",
+                    selected: false,
                 },
                 {
                     name: "La Forza",
                     image: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Visconti-sforza-11-fortitude.jpg",
+                    selected: false,
                 },
                 {
                     name: "L'Appeso",
                     image: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Visconti-sforza-12-traitor.jpg",
+                    selected: false,
                 },
                 {
                     name: "La Morte",
                     image: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Visconti-sforza-13-death.jpg",
+                    selected: false,
                 },
                 {
                     name: "La Temperanza",
                     image: "https://upload.wikimedia.org/wikipedia/commons/a/a6/Visconti-sforza-14-temperance.jpg",
+                    selected: false,
                 },
                 {
                     name: "Il Diavolo",
                     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/RWS_Tarot_15_Devil.jpg/800px-RWS_Tarot_15_Devil.jpg",
+                    selected: false,
                 },
                 {
                     name: "La Torre",
                     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/RWS_Tarot_16_Tower.jpg/800px-RWS_Tarot_16_Tower.jpg",
+                    selected: false,
                 },
                 {
                     name: "La Stella",
                     image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Visconti-sforza-17-star.jpg",
+                    selected: false,
                 },
                 {
                     name: "La Luna",
                     image: "https://upload.wikimedia.org/wikipedia/commons/1/10/Visconti-sforza-18-moon.jpg",
+                    selected: false,
                 },
                 {
                     name: "Il Sole",
                     image: "https://upload.wikimedia.org/wikipedia/commons/e/e6/Visconti-sforza-19-sun.jpg",
+                    selected: false,
                 },
                 {
                     name: "Il Giudizio",
                     image: "https://upload.wikimedia.org/wikipedia/commons/6/6d/Visconti-sforza-20-judgment.jpg",
+                    selected: false,
                 },
                 {
                     name: "Il Mondo",
                     image: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Visconti-sforza-21-world.jpg",
+                    selected: false,
                 },
                 {
                     name: "Il Matto",
                     image: "https://upload.wikimedia.org/wikipedia/commons/1/1b/Visconti-sforza-00-fool.jpg",
+                    selected: false,
                 },
-            ]
+            ],
+            playerCards: [],
 
         }
     },
-    methods: {
+    methods: {       //ToDo: gestire array con carte selezionate
+        selectCard(index) {
+            const card = this.deck[index];
+            if (this.playerCards.length < 5 && this.playerCards.includes(card) === false) {
+                this.playerCards.push(card);
+                card.selected = true;
+            } else if (this.playerCards.includes(card) === true) {
+                const index = this.playerCards.indexOf(card);
+                this.playerCards.splice(index, 1);
+                card.selected = false;
+            }
+            console.log(this.playerCards, "this player cards");
+            console.log(card, "card");
+
+        },
         resetPartita() {
             window.location.reload()
         },
@@ -152,7 +189,8 @@ createApp({
                     // Clean up
                     card.removeAttribute('data-md')
 
-                })
+                }
+                )
             })
         }
     },
